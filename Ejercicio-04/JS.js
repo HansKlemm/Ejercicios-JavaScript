a924e9b6e90bc9b61ac135dbf690965e7990d3b1
Hans Klemm <hanslobenstein@gmail.com>
2025-11-29 23:46:40 +0100

    un commit el dia 29.11.25

--- Ejercicio-04/JS.js
+++ Ejercicio-04/JS.js
@@ -1,11 +1,11 @@
 const contenedor = document.getElementById("texto");
 var cadena = "";
-do {
+/*do {
   var texto = prompt('Escriba algo.');
   if(cadena == ""){
     cadena = texto + cadena;
   }else{
     cadena = texto + "-" + cadena;
   }
-} while (confirm('Desea continuar?'));
+} while (confirm('Desea continuar?'));*/
 contenedor.textContent = "Textos Recuperados: " + cadena;
